d39a0faa6aa04fb1d4d78c5269aff1113f681ff2
foxinboxxxx <visagaboy@gmail.com>
2023-12-23 11:10:01 +0100

    Add new templates and functionality

--- flask_tutorial/model.py
+++ flask_tutorial/model.py
@@ -13,8 +13,11 @@ import json
 
 
 def load_db():
-    with open("flashcards_db.json", encoding="UTF-8") as f:
+    with open("flashcards_db.json") as f:
         return json.load(f)
 
+def save_db():
+    with open("flashcards_db.json", 'w') as f:
+        return json.dump(db, f)
 
 db = load_db()
