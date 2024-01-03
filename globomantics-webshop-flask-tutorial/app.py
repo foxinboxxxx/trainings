562a0b27feff13ec77eb5e76c6cc4502fb9b8054
foxinboxxxx <visagaboy@gmail.com>
2024-01-03 21:17:45 +0100

    Add widget and macros for errors

--- globomantics-webshop-flask-tutorial/app.py
+++ globomantics-webshop-flask-tutorial/app.py
@@ -3,7 +3,9 @@ from flask_wtf import FlaskForm, RecaptchaField
 from flask_wtf.file import FileAllowed, FileRequired
 from wtforms import StringField, TextAreaField, SubmitField, SelectField, DecimalField, FileField
 from wtforms.validators import InputRequired, DataRequired, Length, ValidationError
+from wtforms.widgets import Input
 from werkzeug.utils import secure_filename, escape
+from markupsafe import Markup
 import sqlite3
 import os
 import datetime
@@ -18,6 +20,27 @@ app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 app.config["IMAGE_UPLOADS"] = os.path.join(basedir, "uploads")
 # app.config["RECAPTCHA_PUBLIC_KEY"] = ""
 # app.config["RECAPTCHA_PRIVATE_KEY"] = ""
+
+class PriceInput(Input):
+    input_type = "number"
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("id", field.id)
+        kwargs.setdefault("type", self.input_type)
+        kwargs.setdefault("step", "0.01")
+        if "value" not in kwargs:
+            kwargs["value"] = field._value()
+        if "required" not in kwargs and "required" in getattr(field, "flags", []):
+            kwargs["required"] = True
+        return Markup("""<div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                    </div>
+                    <input %s>
+        </div>""" % self.html_params(name=field.name, **kwargs))
+    
+class PriceField(DecimalField):
+    widget = PriceInput()
 
 class BelongsToOtherFieldOption:
     def __init__(self, table, belongs_to, foreign_key=None, message=None):
@@ -65,7 +88,7 @@ class BelongsToOtherFieldOption:
 
 class ItemForm(FlaskForm):
     title       = StringField("Title", validators=[InputRequired("Input is required"), DataRequired("Data is required"), Length(min=5, max=20, message="Length between 5 and 20")])
-    price       = DecimalField("Price")
+    price       = PriceField("Price")
     description = TextAreaField("Description", validators=[InputRequired("Input is required"), DataRequired("Data is required"), Length(min=5, max=40, message="Length between 5 and 40")])
     image       = FileField("Image", validators=[FileRequired(), FileAllowed(app.config["ALLOWED_IMAGE_EXTENSIONS"], "Images only!")])
     # recaptcha   = RecaptchaField()
@@ -133,8 +156,8 @@ def edit_item(item_id):
         form.description.data = item["description"]
         form.price.data       = item["price"]
 
-        if form.errors:
-            flash("{}".format(form.errors), "danger")
+        # if form.errors:
+        #     flash("{}".format(form.errors), "danger")
         return render_template("edit_item.html", item=item, form=form)
 
     return redirect(url_for("home"))
@@ -283,6 +306,8 @@ def new_item():
     c = conn.cursor()
     form = NewItemForm()
 
+    # TODO: Apply custom select field which takes automatically all of the choices from DB
+    # SelectField class - hint
     c.execute("SELECT id, name FROM categories")
     categories = c.fetchall()
     # [(1, 'Food'), (2, 'Technology'), (3, 'Books')] - numbers are integers
@@ -318,8 +343,8 @@ def new_item():
         flash("Item {} has been successfully submitted".format(request.form.get("title")), "success")
         return redirect(url_for("home"))
 
-    if form.errors:
-        flash("{}".format(form.errors), "danger")
+    # if form.errors:
+    #     flash("{}".format(form.errors), "danger")
     return render_template("new_item.html", form=form)
 
 def save_image_upload(image):
