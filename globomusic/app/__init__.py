c24dc28ef2a7e6bc82161600c8f38ae02e9e5b47
foxinboxxxx <visagaboy@gmail.com>
2024-02-28 22:36:18 +0100

    Cleaning application factory

--- globomusic/app/__init__.py
+++ globomusic/app/__init__.py
@@ -1,30 +1,19 @@
 # Imports from Flask
 from flask import Flask
-# Extension for implementing SQLAlchemy ORM
-from flask_sqlalchemy import SQLAlchemy
-# Extension for implementing Flask-Login for authentication
-from flask_login import LoginManager
-# Extension for implementing translations
-from flask_babel import Babel, _
-from flask_babel import lazy_gettext as _l
 # Other imports
 import os
 import config
+# Import global extension variables
+from app.extensions import *
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# extensions as globals
-db = SQLAlchemy()
-#Babel is used for translation
-babel = Babel()
-login_manager = LoginManager()
+app_env = os.environ.get("FLASK_ENV")
 
-def create_app(config_env=""):
+def create_app(config_env=app_env):
     # Context objects pushed to stack automatically by Flask.
     # When the application instance gets created, both stacks are created too.
     # The request context is pushed whenever a new request comes in.
     app = Flask(__name__)
-    if not config_env:
-        config_env = app.env
     app.config.from_object("config.{}Config".format(app.env.capitalize()))
     # app.config.from_mapping(
     #     SECRET_KEY=os.environ.get("FLASK_SECRET_KEY") or "prc9FWjeLYh_KsPGm0vJcg",
@@ -39,16 +28,8 @@ def create_app(config_env=""):
     # DON'T DO THIS EVER EVER!!!!
     # app.config["ENV"] = "testing"
 
-    # Initializing extensions when factory is executing
-    db.init_app(app)
-    babel.init_app(app)
-    login_manager.init_app(app)
-
-
-    login_manager.login_view = "auth.login"
-    login_manager.session_protection = "strong"
-    login_manager.login_message = _l("You need to be logged in to access this page.")
-    login_manager.login_message_category = "danger"
+    # Initializing extensions
+    init_extensions(app)
 
     # Avoid working outside of application context error
     with app.app_context():
@@ -61,7 +42,12 @@ def create_app(config_env=""):
     from app.tour.views import tour
     app.register_blueprint(tour, url_prefix="/tour")
 
-    from app.main.views import page_not_found
+    # Imports for error pages
+    from app.errors import page_not_found
     app.register_error_handler(404, page_not_found)
+
+    # Imports for Jinja filters
+    from app.filters import date_format
+    app.add_template_filter(date_format)
 
     return app
--- globomusic/app/main/views.py
+++ globomusic/app/main/views.py
@@ -5,13 +5,3 @@ main=Blueprint("main", __name__, template_folder="templates")
 @main.route("/")
 def home():
 	return render_template("home.html")
-
-# 404 error handler
-#@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("errors/404.html")
-
-# Date formatting Jinja2 filter
-@current_app.template_filter("date_format")
-def date_format(value, format="%m/%d/%Y"):
-    return value.strftime(format)
