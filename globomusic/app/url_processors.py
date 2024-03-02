b89e7e552075c22329d0687fff84fa5e706fb18d
foxinboxxxx <visagaboy@gmail.com>
2024-03-02 17:43:17 +0100

    Cookie preference of language

--- globomusic/app/url_processors.py
+++ globomusic/app/url_processors.py
@@ -1,6 +1,19 @@
-from flask import Blueprint, g, current_app
+from flask import Blueprint, g, current_app, after_this_request, request
 
 url_processors = Blueprint("url_processors", __name__)
+
+@url_processors.before_app_request
+def before_request():
+    # check if request for static file like CSS or JS
+    # static files dont need lang
+    if request.endpoint is "static":
+        return
+    if request.cookies.get("lang") != g.lang:
+        @after_this_request
+        def set_cookie(response):
+            response.set_cookie("lang", g.lang, max_age=60*60*24*100)
+            return response
+
 
 # Always add app prefix to attach the app instance
 # to avoid exec before view functions to the blueprint
@@ -13,9 +26,17 @@ def pull_lang_code(endpoint, values):
     # processor willl put lang code from route to this g object
     # and get_locate fun will return value to babel
     try:
+        if endpoint is "static":
+            return
         g.lang = values.pop("lang")
     except:
-        g.lang = current_app.config["LANGUAGES"][0]
+        if (
+                request.cookies.get("lang") and
+                request.cookies.get("lang") in current_app.config["LANGUAGES"]
+            ):
+            g.lang = request.cookies.get("lang")
+        else:
+            g.lang = current_app.config["LANGUAGES"][0]
 
 @url_processors.app_url_defaults
 def add_language_code(endpoint, values):
